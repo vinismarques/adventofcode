b593e603e2a5ceac76c1fc923723f4664596f177
Vinícius Marques <vinicius_marques@live.com>
2025-12-07 14:27:35 -0300

    Implement `merge_ranges` for day 5

--- 2025/05/part_1.py
+++ 2025/05/part_1.py
@@ -2,17 +2,33 @@ from dotenv import load_dotenv
 from utils import get_day_data
 
 
+def merge_ranges(ranges: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    if not ranges:
+        return []
+
+    ranges.sort(key=lambda x: x[0])
+
+    merged = []
+    for r in ranges:
+        if not merged or r[0] > merged[-1][1]:
+            merged.append([*r])
+        else:
+            merged[-1][1] = max(merged[-1][1], r[1])
+    return merged
+
+
 def main(data: str) -> None:
     fresh_input, ingredients_input = data.split("\n\n")
     fresh_ranges = []
     for fresh_range in fresh_input.splitlines():
         start, end = map(int, fresh_range.split("-"))
         fresh_ranges.append((start, end))
+    merged_ranges = merge_ranges(fresh_ranges)
     ingredients = {int(i) for i in ingredients_input.splitlines()}
 
     total_fresh = 0
     for i in ingredients:
-        for r in fresh_ranges:
+        for r in merged_ranges:
             if r[0] <= i <= r[1]:
                 total_fresh += 1
                 break
